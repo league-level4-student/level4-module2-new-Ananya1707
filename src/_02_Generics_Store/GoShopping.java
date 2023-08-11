cd56abfe17fb8e6c68f5aae15a47f28f7633eef4
league <league@imac-14.attlocal.net>
2023-08-10 18:28:12 -0700

    working on Generics Store and Step 9

--- src/_02_Generics_Store/GoShopping.java
+++ src/_02_Generics_Store/GoShopping.java
@@ -10,17 +10,29 @@ public class GoShopping {
 	//    Clothing and Toy are NonFood objects.
 	
 	public static void main(String[] args) {
+		/*
+		 
 		// 2. Create a Cart object of type Candy
+		Cart<Candy> candyCart = new Cart<Candy>();
 		
 		// 3. Create another Cart object of type Cereal
+		Cart<Cereal> cerealCart = new Cart <Cereal>();
 		
 		// 4. Add a few items to EACH cart
 		
-		
+		candyCart.add(new Candy());
+		candyCart.add(new Candy());
+		candyCart.add(new Candy());
+		cerealCart.add(new Cereal());
+		cerealCart.add(new Cereal());
+		cerealCart.add(new Cereal());
 		
 		
 		// 5. Call the showCart() method on EACH cart
+		candyCart.showCart();
+		cerealCart.showCart();
 		
+		*/
 		
 		// 6. Run the code
 		
@@ -28,7 +40,20 @@ public class GoShopping {
 		
 		// 8. Repeat steps 2-5 for a Clothing Cart and a Toy Cart
 		
+		Cart<Clothing> clothingCart = new Cart<Clothing>();
+		Cart<Toy> toyCart = new Cart <Toy>();
 		
+		clothingCart.add(new Clothing());
+		clothingCart.add(new Clothing());
+		clothingCart.add(new Clothing());
+		
+		toyCart.add(new Cereal());
+		toyCart.add(new Cereal());
+		toyCart.add(new Cereal());
+				
+				
+		clothingCart.showCart();
+		toyCart.showCart();
 		
 		
 		// Why are there errors?
